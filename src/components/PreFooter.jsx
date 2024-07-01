0a67e108e74ae9d26ff570443404027047d513af
Movses5555 <58047321+Movses5555@users.noreply.github.com>
2024-07-01 01:55:37 -0400

    changed styles

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -13,19 +13,19 @@ export const PreFooter = () => {
       </div>
       <div className='w-[497px] flex justify-between gap-[120px]'>
         <div className='w-[54px]'>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Home</p>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Problem</p>
-          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Solution</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Home</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Problem</p>
+          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Solution</p>
         </div>
         <div className='w-[79px]'>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Features</p>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Pricing</p>
-          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Get in touch</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Features</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Pricing</p>
+          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Get in touch</p>
         </div>
         <div className='w-[124px]'>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Privacy Policy</p>
-          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Terms & Conditions</p>
-          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.5px]'>Refund Policy</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Privacy Policy</p>
+          <p className='mb-8 text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Terms & Conditions</p>
+          <p className='text-withe font-medium text-[14px] leading-[10px] tracking-[-0.7px]'>Refund Policy</p>
         </div>
       </div>
     </div>
