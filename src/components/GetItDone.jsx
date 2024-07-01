feaf780d361982618d6dda2bd5e2f580ed9dd08f
Movses5555 <58047321+Movses5555@users.noreply.github.com>
2024-07-01 01:39:54 -0400

    changed styles

--- src/components/GetItDone.jsx
+++ src/components/GetItDone.jsx
@@ -3,14 +3,13 @@ export const GetItDone = () => {
   return (
     <div className="max-w-[1248px] h-[303px] bg-[#CDFF05] mx-auto w-full h-full mt-[146px] flex items-center justify-between rounded-[12px] ">
       <div className='w-full flex flex-col items-center justify-center'>
-        <div>
+        <div className="mt-[54px]">
           <span className='uppercase text-xs text-[#090909] font-semibold leading-2'>
             // get it done
           </span>
         </div>
         <p className='mt-[29px] text-black text-6xl leading-[42px] font-medium'>Ready to get started?</p>
-
-        <div className='flex gap-[10px] mt-16'>
+        <div className='flex gap-[10px] mt-16 mb-[54px]'>
           <button
             type="button" 
             className="uppercase bg-[#090909] text-[#CDFF05] text-xs font-semibold rounded-full leading-2 px-4 py-3 text-center"
