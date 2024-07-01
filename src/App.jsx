116e6a23411641dfd6e39b7e20d38439698d004d
Movses5555 <58047321+Movses5555@users.noreply.github.com>
2024-07-01 04:01:04 -0400

    changed styles

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,9 @@ function App() {
         alt="mask_2"
         className='absolute top-[2350px] left-0 w-full h-auto z-0 max-w-full max-h-[1860px] object-cover object-center'
       />
-      <div className={`absolute top-[${height}px] left-0 w-full h-auto z-0 max-w-full max-h-[1860px] object-cover object-center overflow-hidden`}>
+      <div 
+        style={{top: height + "px"}}
+        className={`absolute left-0 w-full h-auto z-0 max-w-full max-h-[1860px] object-cover object-center overflow-hidden`}>
         <img
           src={Mask3}
           alt="mask_3"
