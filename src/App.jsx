614c4ab03b0eb1a24d0579d01607c07b53beb1ef
Movses5555 <58047321+Movses5555@users.noreply.github.com>
2024-07-01 03:53:09 -0400

    changed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import { Header } from './components/Header';
 import { Banner } from './components/Banner';
 import { Features } from './components/Features';
